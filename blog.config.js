863344c24ad93b5f78750e14f3aec52b56e821c2
mykilijobs <mykilijobs@gmail.com>
2024-10-23 22:18:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '当你踏入财务这个行当，你可能一直想着这么跳出去；当你进入境外财务这个行当，这辈子可能再也没法回头了，纵使苦逼，但你仍然坚持', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '当你踏入财务这个行当,你可能一直想着这么跳出去,当你进入境外财务这个行当,这辈子可能再也没法回头了,纵使苦逼,但你仍然坚持.', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://malldoo.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
